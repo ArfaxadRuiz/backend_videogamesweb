dfe30f39832cfcec03089e110002e9c1d061a238
ArfaxadRuiz <iron_arfa@hotmail.com>
2026-02-25 08:34:08 -0600

    se crea el log in

--- app/users/urls.py
+++ app/users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import UserListCreateView, UserDetailView
+from .views import UserListCreateView, UserDetailView, LoginView
 
 urlpatterns = [
+    path('login/', LoginView.as_view()),
     path('', UserListCreateView.as_view()),
     path('<int:pk>/', UserDetailView.as_view()),
+    
 ]
 
--- app/users/views.py
+++ app/users/views.py
@@ -1,6 +1,11 @@
 from rest_framework import generics
 from .models import User
 from .serializers import UserSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model
 
 class UserListCreateView(generics.ListCreateAPIView):
     queryset = User.objects.all()
@@ -10,3 +15,25 @@ class UserListCreateView(generics.ListCreateAPIView):
 class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+User = get_user_model()
+
+class LoginView(APIView):
+
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            return Response({
+                "id": user.id,
+                "username": user.username,
+                "email": user.email,
+                "avatar": user.avatar
+            })
+        else:
+            return Response(
+                {"error": "Credenciales inválidas"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
